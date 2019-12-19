a3944de12d689b77aac0c3c6127f6d732e5be0c1
chaosannals <chaosannals@outlook.com>
2019-12-19 10:40:34 +0800

    鸡尾酒排序。

--- python/sort/bubble.py
+++ python/sort/bubble.py
@@ -1,3 +1,5 @@
+import math
+
 def bubble_sort_2(raw):
     '''
     双循环结构冒泡排序
@@ -49,8 +51,37 @@ def bubble_sort(raw):
             break
     return raw
 
+def cocktail_sort(raw):
+    '''
+    鸡尾酒排序：合并两次遍历为一次正反两向遍历的冒泡排序
+    '''
+    count = len(raw)
+    for i in range(0, math.ceil(count / 2)):
+        # 正向遍历排序
+        final = True
+        for j in range(i, count - i - 1):
+            if raw[j] > raw[j + 1]:
+                tmp = raw[j]
+                raw[j] = raw[j + 1]
+                raw[j + 1] = tmp
+                final = False
+        if final:
+            break
+        # 反向遍历排序
+        final = True
+        for j in range(count - i - 1, i, -1):
+            if raw[j] < raw[j - 1]:
+                tmp = raw[j]
+                raw[j] = raw[j - 1]
+                raw[j - 1] = tmp
+                final = False
+        if final:
+            break
+    return raw
+
 raw = [3,2,1,5,8,6,3,9,7,8,9,10,11,12]
 print(raw)
 print(bubble_sort_2(raw))
 print(bubble_sort_1(raw))
 print(bubble_sort(raw))
+print(cocktail_sort(raw))
