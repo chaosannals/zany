62b8f030d95d76e56061ea0d649196890060aba0
chaosannals <chaosannals@outlook.com>
2019-12-19 09:50:44 +0800

    选择排序。

---
+++ python/sort/selection.py
@@ -0,0 +1,17 @@
+def selection_sort(raw):
+    '''
+    选中最小置于前的选择排序
+    '''
+    count = len(raw)
+    for i in range(0, count - 1):
+        minIndex = i
+        for j in range(i + 1, count):
+            minIndex = minIndex if raw[minIndex] < raw[j] else j
+        tmp = raw[i]
+        raw[i] = raw[minIndex]
+        raw[minIndex] = tmp
+    return raw
+
+raw = [3,2,1,5,8,6,3,9,7,8,9,10,11,12]
+print(raw)
+print(selection_sort(raw))
