a24e39ab77c83a3b76b43c8e542b34142e08e103
chaosannals <chaosannals@outlook.com>
2019-12-21 10:17:12 +0800

    桶排序。

--- python/sort/bucket.py
+++ python/sort/bucket.py
@@ -1,7 +1,10 @@
 import math
 
+
 def bucket_sort(source):
     '''
+    桶排序
+    平均时间复杂度：O（n + m + n(logn - logm))
     '''
     # 取最大最小值
     mi = source[0]
@@ -12,11 +15,11 @@ def bucket_sort(source):
             mx = source[i]
         if source[i] < mi:
             mi = source[i]
-    
+
     # 生成桶
     d = mx - mi
-    bucketCount = count
-    buckets = [None]*bucketCount
+    bucketCount = count # 桶数量
+    buckets = [[]]*bucketCount
     for i in range(0, bucketCount):
         buckets[i] = []
 
@@ -24,11 +27,11 @@ def bucket_sort(source):
     for i in range(0, count):
         n = math.floor((source[i] - mi) * (bucketCount - 1) / d)
         buckets[n].append(source[i])
-    
+
     # 每个桶做排序
     for i in range(0, bucketCount):
         buckets[i].sort()
-    
+
     # 把排序好的数据填入
     index = 0
     target = [0]*count
@@ -37,6 +40,7 @@ def bucket_sort(source):
             target[index] = item
             index += 1
     return target
+
 
 source = [3, 2, 1, 5, 8, 6, 3, 9, 7, 8, 9, 10, 11, 12, 3]
 print(source)
