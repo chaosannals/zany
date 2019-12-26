c0bf8bbb64f409aa19de8901f1130f70a3115c79
chaosannals <chaosannals@outlook.com>
2019-12-26 13:38:33 +0800

    哈希表重置大小。

--- python/data/hash_map.py
+++ python/data/hash_map.py
@@ -10,6 +10,20 @@ class IntHashMap:
         self.mark = (1 << size) - 1
         self.data = [None]*(1 << size)
 
+    def resize(self, size):
+        '''
+        '''
+        self.size = size
+        self.mark = (1 << size) - 1
+        buffer = self.data
+        self.data = [None]*(1 << size)
+        for i in buffer:
+            if isinstance(i, list):
+                for j in i:
+                    self.put(j[0], j[1])
+            elif i != None:
+                self.put(i[0], i[1])
+
     def hash(self, key):
         '''
         '''
@@ -50,6 +64,11 @@ hm = IntHashMap()
 hm.put(3, 'aaaa')
 hm.put(12, 'bbbb')
 hm.put(35, 'cccc')
+print(hm.data)
+print(hm.get(3))
+print(hm.get(12))
+print(hm.get(35))
+hm.resize(6)
 print(hm.data)
 print(hm.get(3))
 print(hm.get(12))
