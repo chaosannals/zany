23165ef9789ed3c778eb54a2b5451401d526fcfc
chaosannals <chaosannals@outlook.com>
2020-04-20 10:40:43 +0800

    清理。

--- js/id.js
+++
@@ -1,8 +0,0 @@
-function makeId(length) {
-    let result = [];
-    for (let i = 0; i < length; ++i) {
-        let v = Math.floor(Math.random() * 16);
-        result.push(v.toString(16));
-    }
-    return result.join();
-}
