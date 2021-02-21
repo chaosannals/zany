050454fd9e0908680c546ce2a8434500c7d1c57a
chenshenchao <chenshenchao@outlook.com>
2021-02-21 14:06:43 +0800

    hacit

---
+++ msys2/vs2019.cmd
@@ -0,0 +1,4 @@
+@set MSYS2_SHELL="E:\MSYS64\msys2_shell.cmd"
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
+@%comspec% /c "%MSYS2_SHELL% -mingw64 -here -full-path"
+
