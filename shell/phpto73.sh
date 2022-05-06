4d4cdc464ebcc1557f86ba9e8ff69f09e7e98f5b
chaosannals <chaosannals@outlook.com>
2022-05-06 13:43:43 +0800

    php

---
+++ shell/phpto73.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -sf /www/server/php/73/bin/php /usr/bin/php
+ln -sf /www/server/php/73/sbin/php-fpm /usr/bin/php-fpm
+ln -sf /www/server/php/73/bin/phpize /usr/bin/phpize
+ln -sf /www/server/php/73/bin/pear /usr/bin/pear
+ln -sf /www/server/php/73/bin/pecl /usr/bin/pecl
---
+++ shell/phpto80.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -sf /www/server/php/80/bin/php /usr/bin/php
+ln -sf /www/server/php/80/sbin/php-fpm /usr/bin/php-fpm
+ln -sf /www/server/php/80/bin/phpize /usr/bin/phpize
+ln -sf /www/server/php/80/bin/pear /usr/bin/pear
+ln -sf /www/server/php/80/bin/pecl /usr/bin/pecl
